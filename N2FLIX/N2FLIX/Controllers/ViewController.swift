49501a1e063da9ea6dd0801a4ff3049fd14932e1
Chynmn <chynmn0523@gmail.com>
2024-04-22 15:02:07 +0900

    [chore] Create Project

---
+++ N2FLIX/N2FLIX/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  N2FLIX
+//
+//  Created by /Chynmn/M1 pro—̳͟͞͞♡ on 4/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
