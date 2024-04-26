80761cea36604a99d8fde04133e816731d10a1fe
Chynmn <chynmn0523@gmail.com>
2024-04-26 15:18:50 +0900

    [chore] merge #7

--- N2FLIX/N2FLIX/Controllers/WishListViewController.swift
+++ N2FLIX/N2FLIX/Controllers/WishListViewController.swift
@@ -1,38 +1,85 @@
+////
+////  WishListViewController.swift
+////  N2FLIX
+////
+////  Created by Jeong-bok Lee on 4/24/24.
+////
 //
-//  WishListViewController.swift
-//  N2FLIX
+//import UIKit
 //
-//  Created by Jeong-bok Lee on 4/24/24.
-//
-
-import UIKit
-
-class WishListViewController: UITableViewController {
-    
-    var wishList: [Movie] = []
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Cell.xib 파일에 등록된 셀을 사용하기 위해 register 메서드를 호출합니다.
-        let nib = UINib(nibName: "Cell", bundle: nil)
-        tableView.register(nib, forCellReuseIdentifier: "Cell")
-        
-        // 상단 여백 추가
-        tableView.contentInset = UIEdgeInsets(top: 16.0, left: 0, bottom: 0, right: 0)
-        
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return wishList.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
-        let movie = wishList[indexPath.row]
-        cell.textLabel?.text = movie.title
-        
-        return cell
-    }
-}
+//// 위시리스트 뷰 컨트롤러 클래스
+//class WishListViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+//    // 섹션당 아이템 수를 반환하는 함수
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return wishListMovies.count  // 위시리스트 영화의 수를 반환
+//    }
+//    
+//    // 각 아이템에 대한 셀을 반환하는 함수
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WishListCell", for: indexPath) as! WishListCollectionViewCell
+//        let movie = wishListMovies[indexPath.row]
+//        cell.configure(with: movie)
+//        return cell
+//    }
+//    
+//    // 컬렉션 뷰 아웃렛
+//    @IBOutlet weak var collectionView: UICollectionView!
+//  
+//    // 컬렉션 뷰의 데이터 소스
+//    var wishListMovies: [Movie] = []
+//  
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    
+//        // 컬렉션 뷰 설정
+//        collectionView.dataSource = self
+//        collectionView.delegate = self
+//    
+//        // 셀 등록
+//        collectionView.register(UINib(nibName: "WishListCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "WishListCell")
+//    
+//        // 위시리스트 영화 가져오기
+//        fetchWishListMovies()
+//    
+//        // 뒤로 가기 버튼 설정
+//        setupBackButton()
+//    }
+//  
+//    // UserDefaults에서 위시리스트 영화를 가져오는 함수
+//    private func fetchWishListMovies() {
+//        wishListMovies = UserData.shared.wishList // wishListMovies 대신 wishList 사용
+//        collectionView.reloadData()
+//    }
+//  
+//    // 뒤로 가기 버튼 설정 함수
+//    private func setupBackButton() {
+//        let backButtonImage = UIImage(named: "Icon/chevronBack_icon")
+//        let backButton = UIButton(type: .custom)
+//        backButton.setImage(backButtonImage, for: .normal)
+//        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+//        backButton.translatesAutoresizingMaskIntoConstraints = false
+//    
+//        view.addSubview(backButton)
+//    
+//        NSLayoutConstraint.activate([
+//            backButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+//            backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+//            backButton.widthAnchor.constraint(equalToConstant: 24),
+//            backButton.heightAnchor.constraint(equalToConstant: 24)
+//        ])
+//    }
+//  
+//    // 뒤로 가기 버튼 액션
+//    @objc func backButtonTapped() {
+//        navigationController?.popViewController(animated: true)
+//    }
+//  
+//    // 위시리스트에서 영화를 제거하는 함수
+//    func removeFromWishList(_ movieTitle: String) {
+//        if let index = wishListMovies.firstIndex(where: { $0.title == movieTitle }) {
+//            wishListMovies.remove(at: index)
+//            UserData.shared.wishList = wishListMovies // UserData의 wishList 업데이트
+//            collectionView.reloadData()
+//        }
+//    }
+//}
---
+++ N2FLIX/Pods/Target Support Files/Pods-N2FLIX-N2FLIXUITests/Pods-N2FLIX-N2FLIXUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_N2FLIX_N2FLIXUITests : NSObject
+@end
+@implementation PodsDummy_Pods_N2FLIX_N2FLIXUITests
+@end
