8d0c17a371a4824f48723e6bcb698e1992bf40e7
Chynmn <chynmn0523@gmail.com>
2024-04-26 15:01:59 +0900

    [add] 데이터 모델 추가 및 수정

--- N2FLIX/N2FLIX/Models/UserData.swift
+++ N2FLIX/N2FLIX/Models/UserData.swift
@@ -23,72 +23,79 @@ struct Booking: Codable {
 }
 
 struct UserData {
-    static let shared = UserData()
+//    static let shared = UserData()
     
-    private let userDefaults = UserDefaults.standard
+//    private let userDefaults = UserDefaults.standard
+//    
+//    private let defaultProfileImageName = "defaultImage"
     
-    private let defaultProfileImageName = "defaultImage"
+    let userEmail : String
+    let userPW : String
+    var userNickName : String
+    var ticketList : [ReserveTicket]
+    var wishList : [MovieDetailModel]
     
-    var isLoggedIn: Bool {
-        return userDefaults.bool(forKey: "isLoggedIn")
-    }
+//    var isLoggedIn: Bool {
+//        return userDefaults.bool(forKey: "isLoggedIn")
+//    }
+//    
+//    var userID: String? {
+//        get { return userDefaults.string(forKey: "userID") }
+//        set { userDefaults.set(newValue, forKey: "userID") }
+//    }
+//    
+//    var password: String? {
+//        get { return userDefaults.string(forKey: "password") }
+//        set { userDefaults.set(newValue, forKey: "password") }
+//    }
     
-    var userID: String? {
-        get { return userDefaults.string(forKey: "userID") }
-        set { userDefaults.set(newValue, forKey: "userID") }
-    }
+//    var userNickName: String? {
+//        get { return userDefaults.string(forKey: "userNickName") }
+//        set { userDefaults.set(newValue, forKey: "userNickName") }
+//    }
+//    
+//    var ticketList: [Booking] {
+//        get {
+//            guard let data = userDefaults.data(forKey: "bookedMovies"),
+//                  let bookings = try? JSONDecoder().decode([Booking].self, from: data) else {
+//                return []
+//            }
+//            return bookings
+//        }
+//        set {
+//            let data = try? JSONEncoder().encode(newValue)
+//            userDefaults.set(data, forKey: "bookedMovies")
+//        }
+//    }
+//    
+//    var wishList: [Movie] {
+//        get {
+//            guard let data = userDefaults.data(forKey: "wishList"),
+//                  let movies = try? JSONDecoder().decode([Movie].self, from: data) else {
+//                return []
+//            }
+//            return movies
+//        }
+//        set {
+//            let data = try? JSONEncoder().encode(newValue)
+//            userDefaults.set(data, forKey: "wishList")
+//        }
+//    }
     
-    var password: String? {
-        get { return userDefaults.string(forKey: "password") }
-        set { userDefaults.set(newValue, forKey: "password") }
-    }
-    
-    var userNickName: String? {
-        get { return userDefaults.string(forKey: "userNickName") }
-        set { userDefaults.set(newValue, forKey: "userNickName") }
-    }
-    
-    var ticketList: [Booking] {
-        get {
-            guard let data = userDefaults.data(forKey: "bookedMovies"),
-                  let bookings = try? JSONDecoder().decode([Booking].self, from: data) else {
-                return []
-            }
-            return bookings
-        }
-        set {
-            let data = try? JSONEncoder().encode(newValue)
-            userDefaults.set(data, forKey: "bookedMovies")
-        }
-    }
-    
-    var wishList: [Movie] {
-        get {
-            guard let data = userDefaults.data(forKey: "wishList"),
-                  let movies = try? JSONDecoder().decode([Movie].self, from: data) else {
-                return []
-            }
-            return movies
-        }
-        set {
-            let data = try? JSONEncoder().encode(newValue)
-            userDefaults.set(data, forKey: "wishList")
-        }
-    }
-    
-    private init() {
-        // Singleton pattern을 따라 private으로 초기화
-    }
-    
-    mutating func login(userID: String, password: String, userNickName: String) {
-        self.userID = userID
-        self.password = password
-        self.userNickName = userNickName
-        userDefaults.set(true, forKey: "isLoggedIn")
-    }
-    
-    func logout() {
-        userDefaults.set(false, forKey: "isLoggedIn")
-        // 다른 사용자 정보 초기화 등 필요한 작업 수행
-   }
+//    private init() {
+//        // Singleton pattern을 따라 private으로 초기화
+//    }
+//    
+//    mutating func login(userID: String, password: String, userNickName: String) {
+//        self.userID = userID
+//        self.password = password
+//        self.userNickName = userNickName
+//        userDefaults.set(true, forKey: "isLoggedIn")
+//    }
+//    
+//    func logout() {
+//        userDefaults.set(false, forKey: "isLoggedIn")
+//        // 다른 사용자 정보 초기화 등 필요한 작업 수행
+//   }
 }
+
